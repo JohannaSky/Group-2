ab7d48616162c73c6ee5fed3b66560bfe449fdf3
TheDeathAngel95 <helena_9503@hotmail.com>
2016-04-04 16:55:27 +0200

    Here is the Menu

---
+++ Meny/Hobby_projekt/Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Hobby_projekt
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        GameStateManager gameStateManager;
+
+        public static int screenWidth = 1920;
+        public static int screenHeight = 1080;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+       
+        protected override void Initialize()
+        {
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            IsMouseVisible = true;
+
+            gameStateManager = new GameStateManager(Content, graphics);
+        }
+        
+        protected override void UnloadContent()
+        {
+
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape) || gameStateManager.gamestate == GameStateManager.GameState.exit)
+                Exit();
+
+            gameStateManager.Update();
+
+            base.Update(gameTime);
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            gameStateManager.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+            spriteBatch.End();
+            
+        }
+    }
+}
---
+++ Meny/Hobby_projekt/GameStateManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Hobby_projekt
+{
+    class GameStateManager
+    {
+        StartMenu startMenu;
+
+        public enum GameState
+        {
+            startMenu,
+            play,
+            exit
+        }
+        public GameState gamestate = GameState.startMenu;
+
+        public GameStateManager(ContentManager content, GraphicsDeviceManager graphics)
+        {
+            startMenu = new StartMenu(this, graphics);
+            startMenu.LoadMenu(content);
+        }
+
+        public void Update()
+        {
+            switch (gamestate)
+            {
+                case GameState.startMenu:
+                    startMenu.Update();
+                    break;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            switch (gamestate)
+            {
+                case GameState.startMenu:
+                    startMenu.Draw(spriteBatch);
+                    break;
+            }
+        }
+
+    }
+}
